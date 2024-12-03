10f87092a0db767d3ef1ae59470007045932f8f1
Kinjal <kinjalchandera0111@gmail.com>
2024-12-04 05:12:11 +0530

    complete to-do with date

--- lib/db_helper.dart
+++ lib/db_helper.dart
@@ -15,6 +15,15 @@ class DbHelper{
   ///open Db
   Database? mDb;
 
+  static final String TABLE_TASK = "todo";
+  static final String TABLE_ID = "t_id";
+  static final String TABLE_COLUMN_TITLE = "t_title";
+  static final String TABLE_COLUMN_DESC = "t_desc";
+  static final String TABLE_COLUMN_COMPLETED = "t_completed";
+  static final String TABLE_COLUMN_CREATED_AT = "t_created_at";
+  static final String TABLE_COLUMN_COMPLETEDATE = "t_complete_date";
+
+
   ///First initial the Database when database
   Future<Database> initDb()async{
     mDb = mDb ?? await openDb();
@@ -29,23 +38,24 @@ class DbHelper{
     var dbpath = join(dairpath.path,"todo.db");
     return openDatabase(dbpath,version: 1,onCreate: (db,version){
       print("Db created");
-      db.execute("create table todo ( t_id integer primary key autoincrement, t_title text not null, t_desc text not null, t_is_completed integer not null, t_duedate text, t_completedate text)");
+      db.execute("create table $TABLE_TASK ( $TABLE_ID integer primary key autoincrement, $TABLE_COLUMN_TITLE text not null, $TABLE_COLUMN_DESC text not null, $TABLE_COLUMN_COMPLETED integer not null default 0, $TABLE_COLUMN_CREATED_AT text, $TABLE_COLUMN_COMPLETEDATE text)");
     });
   }
 
   ///make function for add Task this function is first call initial db function when initdb is not null so db is open whenever db is created
 
   ///insert query
-  Future<bool> addTask ({required String title,required String desc,required int isCompleted,required String dueDate,required String completeDate})async{
+  Future<bool> addTask ({required String title,required String desc,required String dueDateAt})async{
     Database db = await initDb();
-   int rowseffected = await db.insert("todo", {
-      "t_title" : title,
-      "t_desc" : desc,
-      "t_is_completed" : isCompleted,
-      "t_duedate" : dueDate,
-      "t_completedate" : completeDate,
+   int rowseffected = await db.insert(TABLE_TASK, {
+     TABLE_COLUMN_TITLE : title,
+     TABLE_COLUMN_DESC : desc,
+     TABLE_COLUMN_COMPLETED : 0,
+     TABLE_COLUMN_CREATED_AT : DateTime.now().microsecondsSinceEpoch.toString(),
+     TABLE_COLUMN_COMPLETEDATE : dueDateAt,
     });
    //return true if the insertion was successful
+    print("Rows effected succsesfully");
    return rowseffected>0;
   }
 
@@ -54,9 +64,29 @@ class DbHelper{
   //first call function init
   Database db  = await initDb();
   //db.query for select query function
-  List<Map<String,dynamic>> allTask  = await db.query("todo");
+  List<Map<String,dynamic>> allTask  = await db.query(TABLE_TASK);
 
     return allTask;
+  }
+
+
+  ///Update task
+  Future<bool> updateTask(int id,String title,String desc,bool isCompleted)async{
+    Database db = await initDb();
+   int rowsefected = await db.update(TABLE_TASK, {
+     TABLE_COLUMN_TITLE : title,
+     TABLE_COLUMN_DESC : desc,
+     TABLE_COLUMN_COMPLETED : isCompleted ? 1 :0},
+      where: "$TABLE_ID = ?",whereArgs: ['$id']);
+   print("Rows affected by updated");
+   return rowsefected>0;
+  }
+
+  Future<bool> deleatenote({required int id}) async{
+    Database db = await initDb();
+    int rowseffected =await db.delete(TABLE_TASK,where: "$TABLE_ID = ?" ,whereArgs: ['$id']);
+
+    return rowseffected>0;
   }
 
 }
--- lib/home_page.dart
+++ lib/home_page.dart
@@ -1,34 +1,39 @@
 import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
 import 'package:todo_app/db_helper.dart';
 
-class Homepage extends StatefulWidget{
+class Homepage extends StatefulWidget {
   @override
   State<Homepage> createState() => _HomepageState();
 }
 
 class _HomepageState extends State<Homepage> {
   @override
-DbHelper dbHelper =DbHelper.instance;
+  TextEditingController titilecontroller = TextEditingController();
+  TextEditingController descController = TextEditingController();
+
+  DbHelper dbHelper = DbHelper.instance;
 
   List<Map<String, dynamic>> tasks = [];
+  String dueDate = "";
+  DateFormat dtformate = DateFormat.MMMMEEEEd();
 
   @override
   void initState() {
     super.initState();
-    fetchTasks();
+    getTask();
   }
 
-  void fetchTasks() async{
+  void getTask() async {
     tasks = await dbHelper.fetchalltask();
-    setState(() {
-
-    });
+    print("updated all task list");
+    setState(() {});
   }
 
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('To-Do App'),
+        title: Text('To-Do App'),
         centerTitle: true,
       ),
       body: Column(
@@ -37,37 +42,296 @@ DbHelper dbHelper =DbHelper.instance;
             child: ListView.builder(
               itemCount: tasks.length,
               itemBuilder: (context, index) {
-                final task = tasks[index];
+                var task = tasks[index]; // 'task' refers to a single task (Map) at the current index
                 return ListTile(
-                  title: Text(task['t_title']),
-                  subtitle: Text("Due: ${task['t_duedate']}"),
-                  trailing: Text(task['t_is_completed'] == 1
-                      ? "Completed"
-                      : "Pending"),
+                  leading: Checkbox(
+                      value: task['t_completed'] == 1,
+                      onChanged: (bool? value) async {
+                        if (value != null) {
+                          print("updated checkbox");
+                          String title = task['t_title'];
+                          String desc = task['t_desc'];
+                          await dbHelper.updateTask(
+                              task['t_id'], title, desc, value);
+                          getTask();
+                        } 
+                      }),
+                  title: Text(
+                    task['t_title'],
+                    style: TextStyle(
+                        decoration: task['t_completed'] == 1
+                            ? TextDecoration.lineThrough
+                            : TextDecoration.none),
+                  ),
+                  subtitle: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text("Desc:${task['t_desc']}"),
+                      Text(dtformate.format(DateTime.fromMicrosecondsSinceEpoch(int.parse(tasks[index][DbHelper.TABLE_COLUMN_COMPLETEDATE])))),
+
+                    ],
+                  ),
+                  //
+                  // update and delete icon
+                  //
+                  trailing: Row(
+                    mainAxisSize: MainAxisSize.min,
+                    children: [
+                      IconButton(
+                          onPressed: () {
+                            titilecontroller.text = task["t_title"];
+                            descController.text = task["t_desc"];
+                            //
+                            //
+                            /////Update ShowModel bottomSheet
+                            //
+                            //
+                            showModalBottomSheet(
+                                context: context,
+                                shape: RoundedRectangleBorder(
+                                  borderRadius: BorderRadius.vertical(
+                                      top: Radius.circular(21)),
+                                ),
+                                enableDrag: false,
+                                builder: (_) {
+                                  return Container(
+                                    decoration: BoxDecoration(
+                                      borderRadius: BorderRadius.vertical(
+                                          top: Radius.circular(21)),
+                                    ),
+                                    child: Padding(
+                                      padding: const EdgeInsets.all(8.0),
+                                      child: Column(
+                                        children: [
+                                          Text(
+                                            "UPDATE TASK",
+                                            style: TextStyle(fontSize: 21),
+                                          ),
+                                          SizedBox(
+                                            height: 21,
+                                          ),
+                                          TextField(
+                                            controller: titilecontroller,
+                                            decoration: InputDecoration(
+                                                label: Text("Title*"),
+                                                hintText: "Enter Title",
+                                                focusedBorder:
+                                                    OutlineInputBorder(
+                                                  borderRadius:
+                                                      BorderRadius.circular(20),
+                                                ),
+                                                enabledBorder:
+                                                    OutlineInputBorder(
+                                                  borderRadius:
+                                                      BorderRadius.circular(20),
+                                                )),
+                                          ),
+                                          SizedBox(
+                                            height: 12,
+                                          ),
+                                          TextField(
+                                            controller: descController,
+                                            maxLines: 3,
+                                            decoration: InputDecoration(
+                                                label: Text("Desc*"),
+                                                hintText: "Enter Desc",
+                                                focusedBorder:
+                                                    OutlineInputBorder(
+                                                  borderRadius:
+                                                      BorderRadius.circular(20),
+                                                ),
+                                                enabledBorder:
+                                                    OutlineInputBorder(
+                                                  borderRadius:
+                                                      BorderRadius.circular(20),
+                                                )),
+                                          ),
+                                          SizedBox(
+                                            height: 10,
+                                          ),
+                                          Row(
+                                            mainAxisAlignment:
+                                                MainAxisAlignment.end,
+                                            children: [
+                                              OutlinedButton(
+                                                  onPressed: () async {
+                                                    if (titilecontroller
+                                                            .toString()
+                                                            .isNotEmpty &&
+                                                        descController.toString().isNotEmpty) {
+                                                      bool isCompleted = task['t_completed'] == 1;
+
+                                                      bool check = await dbHelper
+                                                          .updateTask(
+                                                              task['t_id'],
+                                                              titilecontroller.text.toString(), titilecontroller.text.toString(),
+                                                              isCompleted);
+
+                                                      if (check) {
+                                                        Navigator.pop(context);
+                                                        getTask();
+                                                      }
+                                                    }
+                                                  },
+                                                  child: Text("Update")),
+                                              SizedBox(
+                                                width: 5,
+                                              ),
+                                              OutlinedButton(
+                                                  onPressed: () {
+                                                    Navigator.pop(context);
+                                                  },
+                                                  child: Text("Cansel")),
+                                            ],
+                                          ),
+                                        ],
+                                      ),
+                                    ),
+                                  );
+                                });
+                          },
+                          icon: Icon(Icons.edit)),
+                      IconButton(
+                          onPressed: () async {
+                            bool check = await dbHelper.deleatenote(
+                                id: tasks[index]["t_id"]);
+                            if (check) {
+                              getTask();
+                            }
+                          },
+                          icon: Icon(
+                            Icons.delete,
+                            color: Colors.red,
+                          )),
+                    ],
+                  ),
                 );
               },
             ),
           ),
           Padding(
-            padding: const EdgeInsets.all(8.0),
-            child: ElevatedButton(
+            padding: EdgeInsets.all(15.0),
+            child: OutlinedButton(
               onPressed: () async {
-                bool result = await dbHelper.addTask(
-                  title: "Finish Homework",
-                  desc: "Complete the math and science homework.",
-                  isCompleted: 0,
-                  dueDate: "2024-11-20",
-                  completeDate: "",
-                );
-
-                if (result) {
-                  print("Task added successfully!");
-                  fetchTasks();
-                } else {
-                  print("Failed to add task.");
-                }
+                //
+                // showmodelbottomsheet
+                //
+                //
+                titilecontroller.clear();
+                descController.clear();
+                showModalBottomSheet(
+                    shape: RoundedRectangleBorder(
+                      borderRadius:
+                          BorderRadius.vertical(top: Radius.circular(21)),
+                    ),
+                    enableDrag: false,
+                    context: context,
+                    builder: (_) {
+                      return Container(
+                        padding: EdgeInsets.all(13),
+                        height: 500,
+                        width: double.infinity,
+                        child: Column(
+                          children: [
+                            Text(
+                              "ADD TASK",
+                              style: TextStyle(fontSize: 21),
+                            ),
+                            SizedBox(
+                              height: 11,
+                            ),
+                            TextField(
+                              controller: titilecontroller,
+                              decoration: InputDecoration(
+                                  label: Text("Title*"),
+                                  hintText: "Enter Title",
+                                  focusedBorder: OutlineInputBorder(
+                                    borderRadius: BorderRadius.circular(20),
+                                  ),
+                                  enabledBorder: OutlineInputBorder(
+                                    borderRadius: BorderRadius.circular(20),
+                                  )),
+                            ),
+                            SizedBox(
+                              height: 12,
+                            ),
+                            TextField(
+                              controller: descController,
+                              maxLines: 3,
+                              decoration: InputDecoration(
+                                label: Text("Description*"),
+                                hintText: "Enter Desc....",
+                                focusedBorder: OutlineInputBorder(
+                                  borderRadius: BorderRadius.circular(20),
+                                ),
+                                enabledBorder: OutlineInputBorder(
+                                  borderRadius: BorderRadius.circular(20),
+                                ),
+                              ),
+                            ),
+                            SizedBox(
+                              height: 10,
+                            ),
+                            OutlinedButton(
+                                onPressed: ()async{
+                                 DateTime ? SelectedDate = await showDatePicker(context: context,
+                                      firstDate: DateTime.now(),
+                                      lastDate: DateTime(2030, 12, 22));
+                                 print(SelectedDate!.microsecondsSinceEpoch.toString());
+                                 dueDate = SelectedDate.microsecondsSinceEpoch.toString();
+                                },
+                                child: Text("chooseDate")),
+                            SizedBox(
+                              height: 10,
+                            ),
+                            Row(
+                              mainAxisAlignment: MainAxisAlignment.end,
+                              children: [
+                                OutlinedButton(
+                                    onPressed: () async {
+                                      if (titilecontroller.text.isNotEmpty &&
+                                          descController.text.isNotEmpty) {
+                                        bool check = await dbHelper.addTask(
+                                            title: titilecontroller.text.toString(),
+                                            desc: descController.text.toString(),
+                                          dueDateAt: dueDate,
+                                        );
+                                        if (check) {
+                                          titilecontroller.clear();
+                                          descController.clear();
+                                          Navigator.pop(
+                                              context); // Close the bottom sheet
+                                          getTask(); // Refresh list
+                                        }
+                                        /*else {
+                                          ScaffoldMessenger.of(context).showSnackBar(
+                                            SnackBar(content: Text('Failed to add task!')),
+                                          );
+                                        }*/
+                                      }
+                                      /*else {
+                                        ScaffoldMessenger.of(context).showSnackBar(
+                                          SnackBar(content: Text('Title and description are required!')),
+                                        );
+                                      }*/
+                                    },
+                                    child: Text("ADD")),
+                                SizedBox(
+                                  width: 5,
+                                ),
+                                OutlinedButton(
+                                    onPressed: () {
+                                      Navigator.pop(context);
+                                    },
+                                    child: Text("Cansel")),
+                              ],
+                            ),
+                          ],
+                        ),
+                      );
+                    });
               },
-              child: Text("Add Sample Task"),
+              child: Text("Add Task"),
             ),
           ),
         ],
